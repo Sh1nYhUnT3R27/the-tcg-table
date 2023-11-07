cc9da3c0d6c11ed10c85c962108d983ae03d7c24
Sh1nyhUnT3R27 <96213423+Sh1nYhUnT3R27@users.noreply.github.com>
2023-11-06 23:58:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// script.js file 
+// Create a QR code reader
+const qrReader = new QRCode.Reader();
 
-function domReady(fn) { 
-	if ( 
-		document.readyState === "complete" || 
-		document.readyState === "interactive"
-	) { 
-		setTimeout(fn, 1000); 
-	} else { 
-		document.addEventListener("DOMContentLoaded", fn); 
-	} 
-} 
-
-domReady(function () { 
-
-	// If found you qr code 
-	function onScanSuccess(decodeText, decodeResult) { 
-		alert("You Qr is : " + decodeText, decodeResult); 
-	} 
-
-	let htmlscanner = new Html5QrcodeScanner( 
-		"my-qr-reader", 
-		{ fps: 10, qrbos: 250 } 
-	); 
-	htmlscanner.render(onScanSuccess); 
+// Add an event listener for the QR code reader
+qrReader.on('decoded', function(decodedData) {
+    // Add the decoded QR code to the list of scanned codes
+    const scannedCodesList = document.getElementById('scanned-codes');
+    const newScannedCodeListItem = document.createElement('li');
+    newScannedCodeListItem.classList.add('list-group-item');
+    newScannedCodeListItem.textContent = decodedData;
+    scannedCodesList.appendChild(newScannedCodeListItem);
 });
+
+// Add an event listener for the "Copy scanned codes" button
+document.getElementById('copy-scanned-codes').addEventListener('click', function() {
+    // Copy the scanned codes to the clipboard
+    const scannedCodesList = document.getElementById('scanned-codes');
+    const scannedCodesText = scannedCodesList.textContent;
+    navigator.clipboard.writeText(scannedCodesText);
+
+    // Display a message to the user
+    alert('Scanned codes copied to clipboard!');
+});
+
+// Start the QR code reader
+qrReader.start();
