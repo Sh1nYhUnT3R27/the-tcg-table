8d693a9177631de2dc85f244b6e63652f6bb6d01
Sh1nyhUnT3R27 <96213423+Sh1nYhUnT3R27@users.noreply.github.com>
2023-11-07 00:20:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  alert(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
 // Get the QR code info element.
 const qrCodeInfo = document.querySelector('#qr-code-info');
 
