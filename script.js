df621a81e5d504aef9a9d42989f50be19971a410
Sh1nyhUnT3R27 <96213423+Sh1nYhUnT3R27@users.noreply.github.com>
2023-11-13 23:38:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,34 @@
-const qrCodeList = [];
+// Create an empty array to store scanned QR codes
+const scannedQRCodes = [];
 
+// Function to handle successful QR code scans
+function onScanSuccess(decodedText, decodedResult) {
+  // Add the scanned QR code to the list
+  scannedQRCodes.push(decodedText);
 
-
-function onScanSuccess(qrCodeText, qrCodeResult) {
-  // Add the scanned QR code to the list of scanned QR codes.
-  qrCodeList.push(qrCodeText);
-
-  // Display an alert with the decoded QR code text and result.
-  alert(`Code matched = ${qrCodeText}`, qrCodeResult);
+  // Update the QR code list UI
+  const qrCodeListItems = document.getElementById('qr-code-list-items');
+  const newListItem = document.createElement('li');
+  newListItem.textContent = decodedText;
+  qrCodeListItems.appendChild(newListItem);
 }
 
+// Function to copy the QR code list to clipboard
 function copyQrCodeListToClipboard() {
-  // Get the text of the QR code list.
-  const qrCodeListText = qrCodeList.join('\n');
+  // Convert the scanned QR code list to a string
+  const qrCodeListText = scannedQRCodes.join('\n');
 
-  // Create a new TextRange object.
-  const textRange = document.createRange();
+  // Copy the QR code list text to the clipboard
+  navigator.clipboard.writeText(qrCodeListText);
 
-  // Select the QR code list text.
-  textRange.selectNodeContents(document.getElementById('qr-code-list-items'));
-
-  // Add the QR code list text to the clipboard.
-  window.getSelection().addRange(textRange);
-
-  // Copy the QR code list text to the clipboard.
-  document.execCommand('copy');
-
-  // Remove the TextRange object from the DOM.
-  textRange.detach();
-
-  // Display an alert to let the user know that the QR code list has been copied to the clipboard.
+  // Display an alert message
   alert('QR code list copied to clipboard!');
 }
 
-
-// JavaScript function to handle failed QR code scans.
-function onScanFailure(error) {
-  // Log a warning message to the console with the scan error.
-  console.warn(`Code scan error = ${error}`);
-}
-
-// Create a new Html5QrcodeScanner object.
+// Initialize the QR code scanner
 const html5QrcodeScanner = new Html5QrcodeScanner(
-  "reader",
+  'reader',
   { fps: 10, qrbox: { width: 250, height: 250 } },
   /* verbose= */ false
 );
-
-// Render the QR code scanner to the DOM element with the ID "reader".
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-
-// Get the QR code info element from the DOM.
-const qrCodeInfo = document.querySelector('#qr-code-info');
-
-// Listen for the QR code scan event.
-document.querySelector('#reader').addEventListener('scan', function(event) {
-  // Get the QR code type and contents from the event object.
-  const qrCodeType = event.detail.type;
-  const qrCodeContents = event.detail.data;
-
-  // Update the QR code info element with the QR code type and contents.
-  qrCodeInfo.querySelector('#qr-code-type').textContent = `QR Code Type: ${qrCodeType}`;
-  qrCodeInfo.querySelector('#qr-code-contents').textContent = `QR Code Contents: ${qrCodeContents}`;
-});
