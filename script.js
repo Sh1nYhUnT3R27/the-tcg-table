465c202575e117938413a93b5e68373a1e2bef72
Sh1nyhUnT3R27 <96213423+Sh1nYhUnT3R27@users.noreply.github.com>
2024-06-19 15:59:17 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navList = document.querySelector('.nav-list');
+
+  navToggle.addEventListener('click', function() {
+    navList.classList.toggle('active');
+  });
+});
